9f0a48251422be292b5a5751c820d58a599dc523
junjie <1287550045@qq.com>
2016-04-27 11:22:13 +0800

    测试

---
+++ app/src/main/java/com/example/benben/mytool/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.example.benben.mytool;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by benben on 2016/4/27.
+ */
+public class BaseActivity extends AppCompatActivity {
+}
